12d0f70e45f821f934f6f942631e0c764a07f1d3
edgardobriban <dobriban@stanford.edu>
2015-04-26 18:11:14 -0700

    readme

--- Experiments/Examples/example.m
+++ Experiments/Examples/example.m
@@ -14,20 +14,18 @@ fprintf(['Saved Results to ' filename '\n']);
 %% Long example:  mixture of uniform + arithmetic model
 
 gap = 0.01;
-t_min = 1;
-t_max = 10;
+t_min = 2;
+t_max = 11;
 K = 10;
 [t,w]  = arithmetic_model(K, t_min,t_max,gap);
 w = 1/2*w;
-t = 1+ t;
 
 r = [0.5 1.5];
 w_int = 1/2;
 
 epsi = 10^(-5);
-num_gp = 5*1e1;
 gamma = 0.01;
-[grid, density,~, ~, mass_at_0] =  atomic(t, gamma, w, r,w_int,epsi);
+[grid, density,~, ~, ~, K_hat, l_hat, u_hat] =  atomic(t, gamma, w, r,w_int,epsi);
 
 %empirical eigenvalues
 rng(0);
@@ -42,8 +40,7 @@ width = locations(2)-locations(1);
 heights = heights / max(heights);
 
 %plot
-figure
-hold on
+figure, hold on
 bar(locations,heights);
 plot(grid, density/max(density),'r','LineWidth',4)
 set(gca,'fontsize',14)
