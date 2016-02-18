7c15ad1a562b59699b004a1011681793780825c7
Edgar Dobriban <dobriban@stanford.edu>
2016-02-18 00:44:55 -0800

    additional output for compute_esd_ode
    Add the following:
    grid_large,v_large - positive entries of the grid, with their v values
    a,b - intervals where v is increasing

--- Code/compute_esd_ode.m
+++ Code/compute_esd_ode.m
@@ -28,16 +28,24 @@ function [grid, density, m, v, mass_at_0, K_hat, l_hat, u_hat, v_x, f_hat,grid_l
 % K_hat - numerical approximation to number of disjoint clusters of support
 % l_hat - lower endpoints of support intervals; real vector of size K_hat;
 % u_hat - upper endpoints of support intervals; real vector of size K_hat;
-% x - grid points within support intervals where density is approximated;
-%       real matrix of size M x K_hat; column i contains the i-th support
-%       interval
-% f_hat - numerical approximation of density on grid x; same format as x
+% v_x - grid points within support intervals where density is approximated;
+%       K_hat x 1 cell array; cell i contains the i-th support interval
+%        (these are a superset of the indices in grid; and they are
+%        provided for convenience in some applications)
+% f_hat - numerical approximation of density on grid v_x; same format as v_x
 % grid_large,v_large - positive entries of the grid, with their v values
+%   (these are supersets of grid and v; of the same length)
 % a,b - intervals where v is increasing
 
 if ~exist('epsilon','var')
     epsilon = 1e-4;
 end
+%set lower bound on epsilon
+%the limitation comes from the ODE solver, which ends up working with 
+%2x the precision of epsilon.
+%and currently this is limited to 16 digits
+epsilon = max(epsilon, 1e-8);
+
 delta_v = sqrt(epsilon);
 
 p = length(t);
@@ -78,6 +86,10 @@ num_clus = 1;
 z_l_endpoints(1) = 0;
 if (gamma <1) %if need to look at lower half
     v_L = min(B) -1;
+    %if epsilon is so large that the grid is empty, make it smaller
+    while (v_L+epsilon)>=(min(B)-epsilon)
+        epsilon = epsilon/2;
+    end
     switch edge_finding
         case 'grid'
             v= linspace(v_L+epsilon, min(B)-epsilon, M);
@@ -88,7 +100,7 @@ if (gamma <1) %if need to look at lower half
                 v= linspace(v_L+epsilon, min(B)-epsilon, M);
                 [grid,~,~,maxz,ind_max] = evaluate_inverse_ST(t,w,gamma, v);
             end
-            z_u_endpoints(num_clus) = maxz;
+            z_u_endpoints(num_clus) = maxz; 
             num_grid_points(num_clus) = ind_max; %number of grid points in first interval
             z_grid(num_clus,1:ind_max) = grid(1:ind_max);
             v_grid(num_clus,1:ind_max) = v(1:ind_max);
@@ -111,6 +123,10 @@ end
 for i = 1:num_unique-1
     v_L = B(i); %lower endpoint -1/t
     v_U = B(i+1); %upper endpoint -1/t
+    %if epsilon is so large that the grid is empty, make it smaller
+    while (v_L+epsilon)>=(v_U-epsilon)
+        epsilon = epsilon/2;
+    end
     %Find the edges of the spectrum
     switch edge_finding
         case 'grid'
@@ -161,6 +177,10 @@ end
 %last interval between -1/t & 0
 v_L = B(num_unique);
 v_U = 0;
+%if epsilon is so large that the grid is empty, make it smaller
+while (v_L+epsilon)>=(v_U-epsilon)
+    epsilon = epsilon/2;
+end
 switch edge_finding
     case 'grid'
         v= linspace(v_L+epsilon, v_U-epsilon, M); %adaptive grid-forming
@@ -192,6 +212,10 @@ num_clus = num_clus+1;
 z_l_endpoints(num_clus) = -Inf;
 if (gamma >1) %if upper half maximum may exceed 0
     v_U = 1;
+    %if epsilon is so large that the grid is empty, make it smaller
+    while (epsilon)>=(v_U-epsilon)
+        epsilon = epsilon/2;
+    end
     switch edge_finding
         case 'grid'
             v= linspace(epsilon, v_U-epsilon, M);
@@ -220,6 +244,10 @@ if (gamma >1) %if upper half maximum may exceed 0
 else %if gamma<1, this interval gives me the whole region m>0;
     %know that the function is strictly increasing in this case
     v_U = M;
+    %if epsilon is so large that the grid is empty, make it smaller
+    while (epsilon)>=(v_U-epsilon)
+        epsilon = epsilon/2;
+    end
     v= linspace(epsilon, v_U-epsilon, M);
     [grid,~,~,~,~] = evaluate_inverse_ST(t,w,gamma, v);
     z_u_endpoints(num_clus) = 0;
@@ -241,14 +269,18 @@ v_grid = v_grid(1:num_clus,:);
 % First Stitch together the real ST
 % pre-allocate grids
 % imag points + real points
-grid_length = (num_clus-2)*M + sum(num_grid_points);
-v = zeros(grid_length,1);
-grid = zeros(grid_length,1);
+
+%changed: do not preallocate grid---because we are forming it adaptively
+%grid_length = (num_clus-2)*M + sum(num_grid_points);
+%v = zeros(grid_length,1);
+%grid = zeros(grid_length,1);
 K_hat = num_clus-2;
 l_hat =zeros(K_hat,1);
 u_hat =zeros(K_hat,1);
-v_x = zeros(M,K_hat);
-f_hat = zeros(M,K_hat);
+%v_x = zeros(M,K_hat);
+%f_hat = zeros(M,K_hat);
+v_x = cell(K_hat,1);
+f_hat =cell(K_hat,1);
 a = zeros(K_hat+1,1);
 b = zeros(K_hat+1,1);
 
@@ -279,16 +311,16 @@ end
 
 %store the increasing intervals
 for i=2:num_clus-1
-ind_ep =ind_ep+1;
-a(ind_ep) = v_grid(i, 1);
-b(ind_ep) = v_grid(i, num_grid_points(i));
+    ind_ep =ind_ep+1;
+    a(ind_ep) = v_grid(i, 1);
+    b(ind_ep) = v_grid(i, num_grid_points(i));
 end
-  
+
 %functions for ODE solving
 MP_diff = @(time,v) (1/v^2 - gamma* sum( w.*(t.^(-1) + v).^(-2)))^(-1);
 ep = epsilon;
 options = odeset('RelTol', ep, 'AbsTol',ep);
- 
+
 %from now on, can handle all points in a uniform way
 %have num_clus-2 pairs of intervals left: look at the support first, then
 %outside
@@ -301,8 +333,11 @@ for i=2:num_clus-1
     u_hat(i-1) = endpoint_2;
     
     %the grid within the i-th support interval
-    grid_current = linspace(endpoint_1,endpoint_2,M)';
-    v_x(:,i-1) = grid_current;
+    %adaptively set the grid length to have an accuracy approximately
+    %sqrt(ep) within the support interval
+    M_curr = floor((endpoint_2-endpoint_1)/sqrt(epsilon))+1;
+    grid_current = linspace(endpoint_1,endpoint_2,M_curr)';
+    v_x(i-1) = {grid_current};
     
     %find starting point for ODE using iterative method:
     [~,~,v_start] =  compute_esd_fp(t,w,gamma,epsilon, grid_current(1));
@@ -311,10 +346,10 @@ for i=2:num_clus-1
     [~,v0] = ode45(MP_diff,grid_current,v_start,options);
     
     %set the output
-    grid(ind+1:ind + M) = grid_current;
-    v(ind+1:ind + M) = v0;
-    f_hat(:,i-1) = 1/pi*imag( 1/gamma*v0- (1-1/gamma)./grid_current);
-    ind = ind + M;
+    grid(ind+1:ind + M_curr) = grid_current;
+    v(ind+1:ind + M_curr) = v0;
+    f_hat(i-1) = {1/pi*imag( 1/gamma*v0- (1-1/gamma)./grid_current)};
+    ind = ind + M_curr;
     
     %add in the corresponding interval outside the suppost
     grid(ind + 1:ind+num_grid_points(i)) = z_grid(i, 1:num_grid_points(i));
@@ -328,8 +363,15 @@ grid_large = grid(grid>0);
 v_large = v(grid>0);
 grid = grid(good_ind);
 v = v(good_ind);
+v=transpose(v);
+v_large=transpose(v_large);
 
-%compute the usual ST from the dual ST
+%remove negative jumps in the grid
+ind_neg = ((grid(2:length(grid))-grid(1:length(grid)-1))<=0);
+grid = grid(~ind_neg);
+v = v(~ind_neg);
+
+%compute the usual ST from the companion ST
 m = 1/gamma*v- (1-1/gamma)./grid;
 density = abs(1/pi*imag(m));
 
