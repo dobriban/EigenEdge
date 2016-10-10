02a53051ca9292cd51763a8d2775c9aefb56d09f
Edgar Dobriban <dobriban@stanford.edu>
2016-10-09 19:56:32 -0700

    spiked forward allows arrays

--- Code/standard_spiked_forward.m
+++ Code/standard_spiked_forward.m
@@ -11,13 +11,19 @@ function [lambda,cos_right,cos_left] = standard_spiked_forward(ell,gamma)
 %lambda - asymptotic sample spike location (eigenvalue)
 %cos_right,cos_left - squares of the right/left singular value angles
 
+k  = length(ell);
+lambda = zeros(k,1);
+cos_right  = zeros(k,1);
+cos_left  = zeros(k,1);
 %BBP PT
-if ell>gamma^(1/2) 
-    lambda = (1+ell)*(1+gamma/ell);
-    cos_right = (1-gamma/ell^2)/(1+gamma/ell);
-    cos_left = (1-gamma/ell^2)/(1+1/ell);
-else
-    lambda = (1+gamma^(1/2))^2;
-    cos_right = 0;
-    cos_left = 0;
+for i=1:k
+    if ell(i)>gamma^(1/2)
+        lambda(i) = (1+ell(i))*(1+gamma/ell(i));
+        cos_right(i) = (1-gamma/ell(i)^2)/(1+gamma/ell(i));
+        cos_left(i) = (1-gamma/ell(i)^2)/(1+1/ell(i));
+    else
+        lambda(i) = (1+gamma^(1/2))^2;
+        cos_right(i) = 0;
+        cos_left(i) = 0;
+    end
 end
